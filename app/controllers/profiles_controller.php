3b010cdc7e65f9ef6c0301d2f933ab8c53dfb4ca
Alexander Ganster <alexander.ganster@gmail.com>
2011-06-26 02:51:41 +0200

    remove not needed code from profile controler
    Signed-off-by: Alexander Ganster <alexander.ganster@gmail.com>

--- app/controllers/profiles_controller.php
+++ app/controllers/profiles_controller.php
@@ -24,36 +24,12 @@ class ProfilesController extends AppController {
    }   
 
    function user_login() {
-
    }
 
    function user_logout() {
       $this->Session->setFlash('Good-Bye');
       $this->log( $this->MyAuth->user('username') . "; $this->name; logout: " . $this->data['User']['username'], 'activity');
       $this->redirect($this->MyAuth->logout());
-   }
-
-
-	function user_index() {
-      # get global settings
-      $Setting  = ClassRegistry::init('GlobalSetting');
-      foreach( $Setting->find('all') as $key=>$value ){
-         $content = $value['GlobalSetting'];
-         $settings[ $content['name'] ] = $content['value'] ; 
-      }
-      $this->set('settings', $settings);
-
-      # If form has been submitted
-      if (!empty($this->data) && isset($this->data['User']['searchstring'])) {
-         $this->NoauthRules->recursive = 0;
-         $string = $this->data['User']['searchstring'];
-         $this->set('users', $this->paginate('User',array("User.username LIKE '%$string%' OR User.realname LIKE '%$string%'")));
-      }
-      else {
-         $this->User->recursive = 0;
-         $this->set('users', $this->paginate());
-      }
-
    }
 
    function user_view($id = null) {
@@ -64,47 +40,6 @@ class ProfilesController extends AppController {
       $user= $this->User->read(null, $id);
       $this->set('user', $user);
    }
-
-	function user_edit($id = null) {
-      if (array_key_exists('cancel', $this->params['form'])) {
-         $this->Session->setFlash(__('Canceled', true));
-         $this->redirect($this->Tracker->loadLastPos());
-      }
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid User', true));
-         $this->redirect($this->Tracker->loadLastPos());
-		}
-		if (!empty($this->data)) {
-			if ($this->User->save($this->data)) {
-				$this->Session->setFlash(__('The User has been saved', true));
-            $this->log( $this->MyAuth->user('username') . "; $this->name ; edit: " . $this->data['User']['username'], 'activity');
-				$this->redirect(array('action'=>'index'));
-			} else {
-				$this->Session->setFlash(__('The User could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->User->read(null, $id);
-		}
-		
-      # show location code + name 
-      $locations_all = $this->User->Location->find('all',array(
-         'fields'=>array('Location.id','Location.code','Location.name'),
-         'recursive'=>-1,
-         'conditions'=>array("Location.id NOT" => "1"),
-         'order'=>array(
-            'Location.code',
-      )));
-      # convert array
-      $locations = Set::combine(
-         $locations_all,
-         '{n}.Location.id',
-         array('%s %s','{n}.Location.code','{n}.Location.name')
-      );
-
-		$groups = $this->User->Group->find('list');
-		$this->set(compact('locations','groups'));
-	}
 
    // password validation doesn't work right now
    function user_setPassword($id = null) {
